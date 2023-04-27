b1d7125890074e76423c7c91322f2e5e280db4e8
Tanner DuPree <98507468+waterspinner@users.noreply.github.com>
2023-04-27 14:01:12 -0700

    first commit

---
+++ AdjacentPair.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'superReducedString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function superReducedString(s) {
+    // Write your code here
+    //cycle through string to get first pair of adjacent letters
+    const regex = /([a-zA-Z])\1/;
+    while(regex.test(s)){
+         //remove letters from string
+         s = s.replace(regex, "");
+}         
+    return s.length > 0 ? s : "Empty String";
+    }
+   
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = superReducedString(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
